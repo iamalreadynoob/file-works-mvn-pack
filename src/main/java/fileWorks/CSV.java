923a4d457f8f6bf016ffd12cbb82e0f552927c30
iamalreadynoob <22120205381@ismu.edu.tr>
2023-10-01 13:10:12 +0300

    commit

--- src/main/java/fileWorks/CSV.java
+++ src/main/java/fileWorks/CSV.java
@@ -117,36 +117,57 @@ public class CSV
     {
         String cell = null;
 
+        if (headers.contains(header) && row >= 0 && row < columns.get(0).size())
+        {
+            cell = columns.get(headers.indexOf(header)).get(row);
+            if (cell.isEmpty() || cell.isBlank()) cell = nullSign;
+        }
+
+
         return cell;
     }
 
-    public void setCell(String header, int row)
+    public void setCell(String header, int row, String newVal)
     {
+        if (newVal == null) newVal = nullSign;
 
+        if (headers.contains(header) && row >= 0 && row < columns.get(0).size())
+            columns.get(headers.indexOf(header)).set(row, newVal);
     }
 
     public void deleteCell(String header, int row)
     {
-
+        setCell(header, row, nullSign);
     }
 
     public boolean isNull(String header, int row)
     {
         boolean flag = false;
 
+        String cell = getCell(header, row);
+        if (cell.equals(nullSign)) flag = true;
 
         return flag;
     }
 
     public void deleteRow(int index)
     {
-
+        if (index < columns.get(0).size() && index >= 0)
+            for (int i = 0; i < headers.size(); i++)
+                columns.get(i).remove(index);
     }
 
     public Map<String, String> getMap(String keyHeader, String valueHeader)
     {
         Map<String, String> map = new HashMap<>();
 
+        if (headers.contains(keyHeader) && headers.contains(valueHeader))
+        {
+            ArrayList<String> keyColumn = getColumn(keyHeader);
+            ArrayList<String> valColumn = getColumn(valueHeader);
+
+            //filter them to unique
+        }
 
         return map;
     }
